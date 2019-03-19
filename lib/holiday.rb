7e0af82686281336b0f0011734489d00845254ef
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 03:12:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,10 +53,10 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holidays|
+  holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holidays.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split.map {|holiday| holiday.capitalize!} }: #{supplies.join(", ")}"
+    holiday.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split.map {|holiday| holiday.capitalize!} }: #{supplies.join(", ")}"
     end
   end
 end
