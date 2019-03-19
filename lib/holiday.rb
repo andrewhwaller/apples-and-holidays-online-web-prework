07e9b633e950c81f0ab02e0aa25c4fb350145fd5
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 03:26:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supply|
       holiday if supply.include?("BBQ")
-        end
       end
     end
 end
