0f9296f022ed8f4e5751baaf0424ea0220a2bf28
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 03:09:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "  #{holiday.to_s.split.map {|holiday| holiday.capitalize!} }: #{supplies.join(", ")}"
+      puts "  #{holiday.to_s.split('_').map {|holiday| holiday.capitalize!} }: #{supplies.join(", ")}"
     end
   end
 
