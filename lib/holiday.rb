533c371dc898781c20da7c9d805cf918483523a2
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 03:13:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,23 +52,23 @@ def all_winter_holiday_supplies(holiday_hash)
   winter_supplies
 end
 
-def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    puts "#{season.capitalize}:"
-    holiday.each do |holiday, supplies|
-      puts"  #{holiday.to_s.split.map {|holiday| holiday.capitalize!} }: #{supplies.join(", ")}"
-    end
-  end
-end
-
 # def all_supplies_in_holidays(holiday_hash)
-#   holiday_hash.each do |season, holidays|
+#   holiday_hash.each do |season, holiday|
 #     puts "#{season.capitalize}:"
-#     holidays.each do |holiday, supplies|
-#       puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+#     holiday.each do |holiday, supplies|
+#       puts "  #{holiday.to_s.split.map {|word| word.capitalize!} }: #{supplies.join(", ")}"
 #     end
 #   end
 # end
+
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday, supplies|
+      puts"  #{holiday.to_s.split('_').map {|w| w.capitalize }.join(' ') }: #{supplies.join(", ")}"
+    end
+  end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
