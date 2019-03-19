ba593fb9542ab02096cb380dc1d2605968934177
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 02:27:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holiday|
+    season.each do |holiday, supplies|
+      puts holiday
+    end
+  end
 
 end
 
